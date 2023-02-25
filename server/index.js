9840c3b1a5dbd5494a86c297b2672dd947895d09
Andrey Ryabtsev <ryab@yandex.ru>
2023-02-25 23:57:19 +0300

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
-
+const colors = require('colors');
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+
+const connectDB = require('./config/db');
 const schema = require('./schema/schema');
 
 const port = process.env.PORT || 5050;
 
 const app = express();
+
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
   schema,
